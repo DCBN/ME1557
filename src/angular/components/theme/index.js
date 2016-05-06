e612fef55686ba817c34041333326c52173e6cca
Alex Hoeing <lhoeing@msn.com>
2016-05-07 00:29:04 +0200

    Initial design for /tour/themes

--- src/angular/components/theme/index.js
+++ src/angular/components/theme/index.js
@@ -6,7 +6,9 @@ import ThemeController from './theme.controller';
 
 import './theme.scss';
 
-export default angular.module('app.theme', [uirouter])
+import themeService from '../../services/themes.service.js';
+
+export default angular.module('app.theme', [uirouter, themeService])
     .config(routing)
     .controller('ThemeController', ThemeController)
     .name;
