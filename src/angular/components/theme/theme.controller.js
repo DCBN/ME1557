8af9ef2fd481822853b321595043aa6446b516bc
Alex Hoeing <lhoeing@msn.com>
2016-05-10 13:45:50 +0200

    Theme controller fix

--- src/angular/components/theme/theme.controller.js
+++ src/angular/components/theme/theme.controller.js
@@ -1,14 +1,3 @@
-<<<<<<< HEAD
- class ThemeController {
-     constructor(themeService) {
-         this.themeService = themeService;
- 	      themeService.getAll().then(result => this.themes = result.data);
-     }
- }
- 
- export default ThemeController;
- ThemeController.$inject = ['themeService'];
-=======
 class ThemeController {
     constructor(themeService) {
         this.themeService = themeService;
@@ -18,4 +7,3 @@ class ThemeController {
 
 export default ThemeController;
 ThemeController.$inject = ['themeService'];
->>>>>>> acdee842d4a344a4b1adaea60e28537641e13358
