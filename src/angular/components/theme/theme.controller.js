7e768f1380041ad6cae5ddb54de6862f30fab552
Alex Hoeing <lhoeing@msn.com>
2016-05-10 13:41:50 +0200

    Conflict fix

--- src/angular/components/theme/theme.controller.js
+++ src/angular/components/theme/theme.controller.js
@@ -2,11 +2,6 @@ class ThemeController {
     constructor(themeService) {
         this.themeService = themeService;
 	      themeService.getAll().then(result => this.themes = result.data);
-        this.getThemeInfo();
-    }
-
-    getThemeInfo() {
-        console.log(this.themes);
     }
 }
 
