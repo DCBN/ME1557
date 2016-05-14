75905aa8d574f6f5702a7f2f169c2e3623a97b4b
Alex Hoeing <lhoeing@msn.com>
2016-05-14 21:48:27 +0000

    Enable shuffling of objects

--- routes/api/apiRoutes.js
+++ routes/api/apiRoutes.js
@@ -4,68 +4,74 @@ const themeList = db.theme;
 const objectList = db.object;
 
 module.exports = (app) => {
+
+    // Route for saving themes
     app.route('/saveTheme')
 	.post((req, res, next) => {
-		if(req.body.name && req.body.imgUrl) {
-			const theme = new themeList();
-			theme._id = mongoose.Types.ObjectId();
-			theme.name = req.body.name;
-			theme.imgUrl = req.body.imgUrl;
-			theme.tags = req.body.tags;
-			theme.save();
-			res.status(200).end();
+	    if(req.body.name && req.body.imgUrl) {
+		const theme = new themeList();
+		theme._id = mongoose.Types.ObjectId();
+		theme.name = req.body.name;
+		theme.imgUrl = req.body.imgUrl;
+		theme.tags = req.body.tags;
+		theme.save();
+		res.status(200).end();
 	    } else {
-			next();
+		next();
 	    }
 	});
 
+    // Route for saving objects
     app.route('/saveObject')
 	.post((req, res, next) => {
 	    if(req.body.name && req.body.imgurl && req.body.desc) {
-			const object = new objectList();
-			object._id = mongoose.Types.ObjectId();
-			object.name = req.body.name;
-			object.imgurl = req.body.imgurl;
-			object.desc = req.body.desc;
-			object.tags = req.body.tags;
-			object.save();
-			res.status(200).end();
+		const object = new objectList();
+		object._id = mongoose.Types.ObjectId();
+		object.name = req.body.name;
+		object.imgurl = req.body.imgurl;
+		object.desc = req.body.desc;
+		object.tags = req.body.tags;
+		object.save();
+		res.status(200).end();
 	    } else {
-			next();
+		next();
 	    }
 	});
 
-	app.route('/getThemes')
+    // Route for retrieving all themes
+    app.route('/getThemes')
 	.get((req, res, next) => {
 	    themeList.find((err ,themes) => {
-			if(err) {
-				console.log(err);
-				return false;
-			}
-			if(themes) {
-				res.status(200).json(themes);
-			} else {
-				next();
-			}
-	    });
-	    		    
+		if(err) {
+		    console.log(err);
+		    return false;
+		}
+		if(themes) {
+		    res.status(200).json(themes);
+		} else {
+		    next();
+		}
+	    }); 
 	});
+
+    // Route for retrieving all objects
     app.route('/getObjects')
 	.get((req, res, next) => {
 	    objectList.find((err, objects) => {
-			if(err) {
-				console.log(err);
-				return false;
-			}
-			if(objects) {
-				res.status(200).json(objects);
-			} else {
-				next();
-			}
+		if(err) {
+		    console.log(err);
+		    return false;
+		}
+		if(objects) {
+		    res.status(200).json(objects);
+		    
+		} else {
+		    next();
+		}
 	    });
-	    
 	});
 
+    // Route for retrieving themes by name
     app.route('/getThisTheme')
 	.get((req, res, next) => {
 	    const theme = req.query.theme;
@@ -81,6 +87,7 @@ module.exports = (app) => {
 	    });
 	});
 
+    // Route for retrieving objects by tags
     app.route('/getObjectsByTag')
 	.get((req, res, next) => {
 	    objectList.find({tags: { $in: req.query.tags}}, (err, objects) => {
@@ -88,10 +95,36 @@ module.exports = (app) => {
 		    console.log(err);
 		}
 		if(objects) {
+		    shuffle(objects);
 		    res.status(200).json(objects);
 		} else {
 		    next();
 		}
 	    });
 	});
+
+    // Function for array shuffling
+    var shuffle = (array) => {
+	// Assign variable to the length of the Array
+	let counter = array.length;
+
+	// While counter is larger that zero then run the shuffling
+	while(counter > 0) {
+	    // Pick a random index
+	    let index = Math.floor(Math.random() * counter);
+
+	    // Decrease the counter by 1
+	    counter--;
+
+	    /*
+	     * Assign Array object at counter to temp
+	     * Swap the object at counter with the random index object
+	     */
+	    let temp = array[counter];
+	    array[counter] = array[index];
+	    array[index] = temp;
+	}
+	// Return array shuffled
+	return array;
+    };
 };
