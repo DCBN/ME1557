941f4e886cf55c789a9a817b36e0106d392768da
Alex Hoeing <Alex Hoeing>
2016-05-29 17:08:45 +0200

    Redirect if no session or not done

--- src/angular/components/completed/completed.controller.js
+++ src/angular/components/completed/completed.controller.js
@@ -1,5 +1,12 @@
 class CompletedController {
-
+  constructor($stateParams, $location) {
+    this.param = $stateParams.theme;
+    const session = JSON.parse(window.sessionStorage.getItem(this.param));
+    console.log(session);
+    if(!session || !session.done) {
+      $location.path('/tour/theme');
+    }
+  }
 }
 
 export default CompletedController;
